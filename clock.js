9068173bbcd59fece361b0c2a1c1089c4cf46ad1
Pedro <Pedro@Bradleys-MacBook-Pro.local>
2021-02-13 15:16:34 -0500

    new ipdates

--- clock.js
+++ clock.js
@@ -1,6 +1,3 @@
-
-
-
 function incrementBreak() {
     let breakLength = document.getElementById("break-length");
     if (parseFloat(breakLength.innerText) < 60) {
@@ -33,33 +30,39 @@ function decrementSession() {
         document.getElementById("minutes").innerText = decrementedSessionValue;
     }
 }
-var secondsLeft = 60;
-var minutesLeft = document.getElementById("minutes");
-function toggleOnOff(){
-        var Timer = setInterval(function 
-            function1(){ 
-        document.getElementById("seconds").innerHTML = secondsLeft;
-        
-        secondsLeft -= 1;
-        if(secondsLeft <= 0){
-            clearInterval(Timer);
-            var Timer2 = setInterval(function
-                function2() {
-                document.getElementById("minutes").innerHTML = minutesLeft;
+var clockCount = document.getElementById('start-stop').addEventListener('click', timerOn);
+function timeCounter(arg1, arg2) {
+    var sessionTime = arg1;
+    var breakTime = arg2;
+    var interval = setInterval(function() {
+        arg1 -= 1000;
+        var getDate = new Date();
+        getDate.setTime(arg1);
+        var timer = parseFloat(document.getElementById("minutes").innerHTML);
+        timer.innerHTML = ('0' + getDate.getMinutes()).slice(-2) + ":" + ('0' + getDate.getSeconds()).slice(-2);
 
-                minutesLeft -= 1;
-                if (minutesLeft <= 0){
-                    clearInterval(Timer2);
-                }
-                },1000)
+        if (arg1 === 0) {
+        var snd = new Audio("Audio-1.0mp4");
+        snd.play();
+        var whatIsCounted = document.getElementById("timer-label");
+        whatIsCounted.textContent = whatIsCounted.textContent == "session" ? whatIsCounted.textContent = "break" : whatIsCounted.textContent = "session";
+        clearInterval(interval);
+        timeCounter(breakTime, sessionTime);
         }
-        }, 1000);
-        
-            console.log(countdown);
-        
+    }, 1000);
 
+    // ------ reset button 
+    var resetBttn = document.getElementById('reset').addEventListener('click', resetTimer);
+    function resetTimer() {
+        window.clearInterval(interval);
+        document.getElementById("timer-label").textContent = "session";
+        document.getElementById("minutes").textContent = "25:00";
+    }
 }
 
-function resetClock(){
-    console.log("coñaso")
+function timerOn() {
+    var userBreakTime = Number(document.getElementById('break-length').innerText) * 60000;
+    var userSessionTime = Number(document.getElementById('minutes').innerText) * 60000;
+    document.getElementById("timer-label").textContent = "session";
+    timeCounter(userSessionTime, userBreakTime);
 }
